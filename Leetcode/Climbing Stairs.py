0b182cb9754d81f0853711d957cd96df0b318781
Xinran <lindashi527@gmail.com>
2015-11-27 12:48:16 -0800

    added

---
+++ Leetcode/Climbing Stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        memo = {1:1, 2:2}
+        return self.cilmbStairs_helper(n, memo)
+
+    def cilmbStairs_helper(self, n, memo):
+        if n not in memo:
+            memo[n] = self.cilmbStairs_helper(n - 1, memo) + self.cilmbStairs_helper(n - 2, memo)
+        return memo[n]
+
+        
